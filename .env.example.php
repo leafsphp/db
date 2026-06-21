4ffc482218884f71644ba253bfde4d2c79244030
fadrian06 <franyeradriansanchez@gmail.com>
2026-06-20 23:59:36 -0400

    remove db secrets

--- .env.example.php
+++ .env.example.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 return [
     'DB_CONNECTION' => 'mysql',
-    'DB_HOST' => 'eu-cdbr-west-03.cleardb.net',
-    'DB_USERNAME' => 'b9607a8a6d5ebb',
-    'DB_PASSWORD' => 'cc589b17',
-    'DB_DATABASE' => 'heroku_fb1311a639bb407',
+    'DB_HOST' => '127.0.0.1',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => '',
+    'DB_DATABASE' => 'test',
 ];
